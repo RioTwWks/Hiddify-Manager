b2ba38a9a93154d562532ca1f0f7b972ebcbc415
RioTwWks <ozrkrakodil@gmail.com>
2025-12-04 01:31:29 +0700

    Update: All URLs point to RioTwWks forks
    - .gitmodules: hiddifypanel (not Hiddify-Panel)
    - packages.lock: RioTwWks/{hiddify-sing-box,ssh-liberty-bridge}
    - Scripts: docker-installer, download, utils, hiddify_installer
    - singbox/add_version.sh: RioTwWks URLs
    - telegram/python: RioTwWks/mtprotoproxy
    
    Now fully independent from upstream repositories

--- other/telegram/python/install.sh
+++ other/telegram/python/install.sh
@@ -2,5 +2,5 @@ source /opt/hiddify-manager/common/utils.sh
 install_package python3 python3-uvloop python3-cryptography python3-socks libcap2-bin
 
 useradd --no-create-home -s /usr/sbin/nologin tgproxy
-git clone https://github.com/hiddify/mtprotoproxy
+git clone https://github.com/RioTwWks/mtprotoproxy
 cp config.py mtprotoproxy/config.py
